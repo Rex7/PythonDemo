cd26d20586a12deb86097f1e15fa2d1ab065beaf
Rex7 <regischarles7@gmail.com>
2016-05-18 16:54:16 +0530

    Just String related Function

---
+++ StringManipulation.py
@@ -0,0 +1,13 @@
+#Demo for String function
+
+name="regis charles"
+capital=name.capitalize()
+lowercase=name.lower()
+uppercase=name.upper()
+size=len(name)
+print(size)
+#replace e with x
+changed=name.replace('e','x',3)
+print("Changed name"+changed)
+print("name in capital :",capital,"name in upper case:",uppercase,"name in lower case",lowercase)
+print(name)
