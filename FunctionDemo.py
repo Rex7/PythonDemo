042e928b01a095ffbb6c4e827685077a3c067246
Rex7 <regischarles7@gmail.com>
2016-05-23 18:19:06 +0530

    Function in python

---
+++ FunctionDemo.py
@@ -0,0 +1,23 @@
+#Function type and way it works
+def printmyname(name,age):
+    print("name of the person is:",name,age)
+printmyname(age=22,name="regis")
+#function with defualt argument
+def value(user="admin"):
+    print("Welcome user:",user)
+#passsing no arguments gives the default value set
+value()
+#if argument is passed then the default value is overriden
+value("Rex")
+#function which returns values
+def add(a,b):
+    return a+b
+ans=add(10,69)
+print(ans)
+def anynumber(*av):
+    for i in av:
+        print(i)
+
+#variable argument
+ar=(18,22)
+anynumber(ar)
