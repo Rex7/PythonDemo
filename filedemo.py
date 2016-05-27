4925e1025055efd3a4c5754795342a19fb5bc0b1
Rex7 <regischarles7@gmail.com>
2016-05-27 19:02:08 +0530

    updated file demo program

--- filedemo.py
+++ filedemo.py
@@ -1,8 +1,12 @@
 # file demo
-file=open("regis.txt","w")
+# enter file name for creation
+name=input("Enter file name:?")
+file=open(name,"w")
 msg=input("Enter the msg for the file")
 file.write(msg)
 file.flush()
+
+# reading the file
 reads=open("regis.txt","r").read()
 print(reads)
 file.close()
