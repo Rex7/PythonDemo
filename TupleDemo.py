fa472580fd3c04cc3c998b59c53e4fb76ca72c81
Rex7 <regischarles7@gmail.com>
2016-08-18 22:39:54 +0530

    Just a simple tuple demo for using tuple

---
+++ TupleDemo.py
@@ -0,0 +1,11 @@
+"""
+Tuple Demo
+Tuple is a immutable data structure
+which once created cant be modified
+"""
+tup =(23,45,78,96,69)
+def tuple_data(tup):
+    for x in tup:
+        print("Element: {0}".format(x))
+tuple_data(tup)
+
