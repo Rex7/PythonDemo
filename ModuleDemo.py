a1537a0c2a5848aef12df53d7e6b6a1fd2b62544
Rex7 <regischarles7@gmail.com>
2016-08-22 10:36:36 +0530

    updated with default argument method

--- ModuleDemo.py
+++ ModuleDemo.py
@@ -3,8 +3,11 @@ pass_word = 9821572187
 
 
 def usercheck(name,password):
-    if name==user and pass_word==password:
+    if name == user and pass_word == password:
         print("Welcome master rex")
+        default_argument(password,name)
     else:
         print("Sorry your not authorized user")
     return
+def default_argument(password,name="admin"):
+    print("Name :{0} \n Password:{1}".format(name,password))
